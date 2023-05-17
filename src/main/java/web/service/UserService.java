2eafde22dc240d6ec3663b38ac1c78936dbc829f
Maria Azarenok <maria_azarenok@outlook.com>
2023-05-17 23:52:07 +0300

    Final

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,14 @@
+package web.service;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public List<User> findAll();
+    public User findOne(int id);
+    public void save(User user);
+    public void update(int id, User updatedUser);
+    public void delete(int id);
+}
